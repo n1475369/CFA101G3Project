6d5c6a790b1fc15e00a649036a886e32a43cfb13
wei <wei_2320@hotmail.com>
2021-07-22 01:12:06 +0800

    index.jsp製作中 版面已刻好

--- src/com/weddingPhoto/controller/WedPhotoServlet.java
+++ src/com/weddingPhoto/controller/WedPhotoServlet.java
@@ -1,38 +1,42 @@
-package com.weddingPhoto.controller;
+package com.weddingphoto.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.weddingPhoto.model.WedService;
-import com.weddingPhoto.model.WedVO;
+import com.weddingphoto.model.WedService;
+import com.weddingphoto.model.WedVO;
 
 
-@WebServlet("/weddingPhoto/wedPhotoServlet")
+@WebServlet("/weddingphoto/wedPhotoServlet")
 public class WedPhotoServlet extends HttpServlet {
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("image/*");
 		Integer wed_id = null;
+		
 		try {
-			wed_id = Integer.parseInt(request.getParameter("wed_id"));
-			WedService wedService = new WedService();
-			WedVO wedVO = wedService.findByPrimaryKey(wed_id);
-			if(wedVO != null) {
+			wed_id = new Integer (request.getParameter("wed_id"));//取得照片ID
+			WedService wedSvc = new WedService();
+			WedVO wedVO = wedSvc.findByPrimaryKey(wed_id);//ID取出對應的VO
+			if(wedVO!=null) {
 				byte[] wed_images = wedVO.getWed_images();
-				response.getOutputStream().write(wed_images);
+				ServletOutputStream out = response.getOutputStream();//輸出
+				out.write(wed_images);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
 	}
 
 }
--- src/com/weddingPhoto/model/WedVO.java
+++ src/com/weddingPhoto/model/WedVO.java
@@ -1,11 +1,23 @@
-package com.weddingPhoto.model;
+package com.weddingphoto.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class WedVO implements Serializable{
+	
+	//婚攝作品集照片
 	private Integer wed_id;
-	private Integer wed_wor_id;
-	private byte[] wed_images;
+	private Integer wed_wor_id;//作品集
+	private byte[] wed_images;//圖
+	public WedVO() {
+
+	}
+	public WedVO(Integer wed_id, Integer wed_wor_id, byte[] wed_images) {
+		super();
+		this.wed_id = wed_id;
+		this.wed_wor_id = wed_wor_id;
+		this.wed_images = wed_images;
+	}
 	public Integer getWed_id() {
 		return wed_id;
 	}
@@ -18,10 +30,17 @@ public class WedVO implements Serializable{
 	public void setWed_wor_id(Integer wed_wor_id) {
 		this.wed_wor_id = wed_wor_id;
 	}
+	
 	public byte[] getWed_images() {
 		return wed_images;
 	}
+	
 	public void setWed_images(byte[] wed_images) {
 		this.wed_images = wed_images;
+	}
+	@Override
+	public String toString() {
+		return "WedVO [wed_id=" + wed_id + ", wed_wor_id=" + wed_wor_id + ", wed_images=" + Arrays.toString(wed_images)
+				+ "]";
 	}
 }
